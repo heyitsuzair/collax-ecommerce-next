df8d4204e1737799e20f9a502859cec1ef8873cf
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-01-12 05:36:33 +0500

    Cart Icon And Page Added

--- src/components/commons/navigation/Navbar.jsx
+++ src/components/commons/navigation/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = ({ minimal }) => {
       <header className="absolute z-[3] flex justify-between items-center p-3 w-[97%] mx-auto right-0 left-0 top-10">
         <div className="left flex items-center md:gap-16 lg:gap-28">
           <Link href={"/"}>
-            <Logo color="blue" />
+            <Logo height={14} color="blue" />
           </Link>
           <div className="hidden md:block">
             <ul className="flex items-center gap-10">
@@ -45,12 +45,26 @@ const Navbar = ({ minimal }) => {
           </div>
         </div>
         <div>
+          <Link href="/cart" className="relative">
+            <div className="absolute -top-8 right-1 bg-yellow-300 w-7 h-7 rounded-full flex items-center justify-center">
+              4
+            </div>
+            <button
+              className="py-3 px-5 rounded-lg shadow-xl lg:shadow-none bg-white hover:bg-yellow-300 transition-all mr-3"
+              type="button"
+            >
+              <i
+                className="fa fa-shopping-cart text-2xl"
+                aria-hidden="true"
+              ></i>
+            </button>
+          </Link>
           <button
-            className="py-4 px-6 rounded-lg shadow-xl md:shadow-none bg-white hover:bg-yellow-300 transition-all"
+            className="py-3 px-5 rounded-lg shadow-xl lg:shadow-none bg-white hover:bg-yellow-300 transition-all"
             type="button"
             onClick={() => setIsCanvasOpen(!isCanvasOpen)}
           >
-            <i className="fa fa-bars text-3xl" aria-hidden="true"></i>
+            <i className="fa fa-bars text-2xl" aria-hidden="true"></i>
           </button>
           <NavbarOffcanvas
             isOpen={isCanvasOpen}
@@ -64,7 +78,7 @@ const Navbar = ({ minimal }) => {
       <header className="sticky top-10 flex justify-between shadow-xl rounded-xl p-3 items-center w-[97%] mx-auto right-0 left-0">
         <div className="left flex items-center md:gap-16 lg:gap-28">
           <Link href={"/"}>
-            <Logo color="blue" />
+            <Logo height={14} color="blue" />
           </Link>
           <div className="hidden md:block">
             <ul className="flex items-center gap-10">
@@ -84,12 +98,26 @@ const Navbar = ({ minimal }) => {
           </div>
         </div>
         <div>
+          <Link href="/cart" className="relative">
+            <div className="absolute -top-8 right-1 bg-yellow-300 w-7 h-7 rounded-full flex items-center justify-center">
+              4
+            </div>
+            <button
+              className="py-3 px-5 rounded-lg bg-white hover:bg-yellow-300 transition-all mr-3"
+              type="button"
+            >
+              <i
+                className="fa fa-shopping-cart text-2xl"
+                aria-hidden="true"
+              ></i>
+            </button>
+          </Link>
           <button
-            className="py-4 px-6 rounded-lg hover:bg-yellow-300 transition-all"
+            className="py-3 px-5 rounded-lg bg-white hover:bg-yellow-300 transition-all"
             type="button"
             onClick={() => setIsCanvasOpen(!isCanvasOpen)}
           >
-            <i className="fa fa-bars text-3xl" aria-hidden="true"></i>
+            <i className="fa fa-bars text-2xl" aria-hidden="true"></i>
           </button>
           <NavbarOffcanvas
             isOpen={isCanvasOpen}
