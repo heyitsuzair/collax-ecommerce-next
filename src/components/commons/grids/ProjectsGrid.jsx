851eb9c527d54c2dcc8627526885df00cbdfedef
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-01-14 04:46:36 +0500

    Our Projects Section Completed

--- src/components/commons/grids/ProjectsGrid.jsx
+++ src/components/commons/grids/ProjectsGrid.jsx
@@ -6,91 +6,126 @@ import Text4Xl from "../text/Text.4xl";
 import TextMd from "../text/Text.md";
 import TextXl from "../text/Text.xl";
 
-const ProjectsGrid = () => {
-  const image = require("../../../img/port1.png");
+const ProjectsGrid = ({ grid }) => {
+  /**
+   * Images
+   */
+  const IMAGE_1 = require("../../../img/" + grid[0].image);
+  const IMAGE_2 = require("../../../img/" + grid[1].image);
+  const IMAGE_3 = require("../../../img/" + grid[2].image);
+
+  /**
+   * Titles
+   */
+  const TITLE_1 = grid[0].title;
+  const TITLE_2 = grid[1].title;
+  const TITLE_3 = grid[2].title;
+
+  /**
+   * Description
+   */
+  const DESCRIPTION_1 = grid[0].description;
+  const DESCRIPTION_2 = grid[1].description;
+  const DESCRIPTION_3 = grid[2].description;
+
+  /**
+   * Button Text
+   */
+  const BTN_TEXT_1 = grid[0].buttonText;
+  const BTN_TEXT_2 = grid[1].buttonText;
+  const BTN_TEXT_3 = grid[2].buttonText;
+
+  /**
+   * Button Icon
+   */
+  const BTN_ICON_1 = grid[0].buttonIcon;
+  const BTN_ICON_2 = grid[1].buttonIcon;
+  const BTN_ICON_3 = grid[2].buttonIcon;
+
+  /**
+   * Link
+   */
+  const LINK_1 = grid[0].link;
+  const LINK_2 = grid[1].link;
+  const LINK_3 = grid[2].link;
 
   return (
     <div class="grid grid-cols-12 gap-6 justify-center">
       <div className="bg-gray-100 flex flex-col gap-6 lg:gap-8 p-3 lg:p-7 col-span-12 lg:col-span-6 rounded-2xl">
         <div className="p-3 lg:p-5 bg-gray-300 rounded-2xl">
           <Image
-            src={image}
+            src={IMAGE_1}
             alt="Loading..."
-            className="rounded-xl w-full h-full"
+            className="rounded-xl w-full h-full object-cover"
           />
         </div>
-        <Text4Xl
-          text="Banking App Management"
-          classes="text-black font-medium"
-        />
-        <TextMd
-          text="At Fellow Coders we specialize in designing, building, shipping..."
-          classes="text-gray-400 font-medium"
-        />
-        <Link href={"/"}>
-          <ScaledButton
-            size="large"
-            text="View Code"
-            icon="fa fa-arrow-right"
-          />
-        </Link>
+        <div className="flex flex-col justify-between gap-6 h-full">
+          <div>
+            <Text4Xl text={TITLE_1} classes="text-black font-medium" />
+            <TextMd
+              text={DESCRIPTION_1}
+              classes="text-gray-400 font-medium mt-4"
+            />
+          </div>
+          <div>
+            <a target="_blank" href={LINK_1} rel="noreferrer">
+              <ScaledButton text={BTN_TEXT_1} icon={BTN_ICON_1} />
+            </a>
+          </div>
+        </div>
       </div>
       <div className="col-span-12 lg:col-span-6">
-        <div className="flex flex-col gap-6 lg:gap-8">
-          <div className="justify-center bg-gray-100 grid grid-cols-12 gap-6 p-3 lg:p-7 col-span-12 lg:col-span-6 rounded-2xl">
+        <div className="flex flex-col h-full justify-between gap-6">
+          <div className="justify-center bg-gray-100 h-full grid grid-cols-12 gap-6 p-3 lg:px-7 lg:py-10 col-span-12 lg:col-span-6 rounded-2xl">
             <div className="p-3 lg:p-5 bg-gray-300 col-span-12 lg:col-span-6 rounded-2xl">
               <Image
-                src={image}
+                src={IMAGE_2}
                 alt="Loading..."
                 className="rounded-xl object-cover h-full w-full"
               />
             </div>
-            <div className="flex flex-col gap-6 col-span-12 lg:col-span-6">
-              <TextXl
-                text="Banking App Management"
-                classes="text-black font-medium"
-              />
-              <TextMd
-                text="At Fellow Coders we specialize in designing, building, shipping..."
-                classes="text-gray-400 font-medium"
-              />
-              <Link href={"/"}>
+            <div className="flex flex-col justify-between gap-6 col-span-12 lg:col-span-6">
+              <div>
+                <TextXl text={TITLE_2} classes="text-black font-medium" />
+                <TextMd
+                  text={DESCRIPTION_2}
+                  classes="text-gray-400 font-medium mt-4"
+                />
+              </div>
+              <a target="_blank" href={LINK_2} rel="noreferrer">
                 <ScaledButton
-                  size="medium"
-                  text="View Code"
+                  text={BTN_TEXT_2}
+                  icon={BTN_ICON_2}
                   textColor="black"
                   buttonColor="bg-white"
-                  icon="fa fa-arrow-right"
                 />
-              </Link>
+              </a>
             </div>
           </div>
-          <div className="justify-center bg-gray-100 grid grid-cols-12 gap-6 p-3 lg:p-7 col-span-12 lg:col-span-6 rounded-2xl">
+          <div className="justify-center bg-gray-100 h-full grid grid-cols-12 gap-6 p-3 lg:px-7 lg:py-10 col-span-12 lg:col-span-6 rounded-2xl">
             <div className="p-3 lg:p-5 bg-gray-300 col-span-12 lg:col-span-6 rounded-2xl">
               <Image
-                src={image}
+                src={IMAGE_3}
                 alt="Loading..."
                 className="rounded-xl object-cover h-full w-full"
               />
             </div>
-            <div className="flex flex-col gap-6 col-span-12 lg:col-span-6">
-              <TextXl
-                text="Banking App Management"
-                classes="text-black font-medium"
-              />
-              <TextMd
-                text="At Fellow Coders we specialize in designing, building, shipping..."
-                classes="text-gray-400 font-medium"
-              />
-              <Link href={"/"}>
+            <div className="flex flex-col justify-between gap-6 col-span-12 lg:col-span-6">
+              <div>
+                <TextXl text={TITLE_3} classes="text-black font-medium" />
+                <TextMd
+                  text={DESCRIPTION_3}
+                  classes="text-gray-400 font-medium mt-4"
+                />
+              </div>
+              <a target="_blank" href={LINK_3} rel="noreferrer">
                 <ScaledButton
-                  size="medium"
-                  text="View Code"
+                  text={BTN_TEXT_3}
+                  icon={BTN_ICON_3}
                   textColor="black"
                   buttonColor="bg-white"
-                  icon="fa fa-arrow-right"
                 />
-              </Link>
+              </a>
             </div>
           </div>
         </div>
--- src/components/pages/home/OurProjects.jsx
+++ src/components/pages/home/OurProjects.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { OurProjectsTabs } from "../../../config";
+import { OurProjectsGrid, OurProjectsTabs } from "../../../config";
 import {
   HeadingCircleLeft,
   ProjectsGrid,
@@ -18,21 +18,21 @@ const OurProjects = () => {
    * Variable To Hold Tabs Content
    */
   const tabsContent = [
-    <ProjectsGrid key={1} text="1" />,
-    <Text2Xl key={2} text="2" />,
-    <Text2Xl key={3} text="3" />,
+    <ProjectsGrid key={1} grid={OurProjectsGrid[0]} />,
+    <ProjectsGrid key={2} grid={OurProjectsGrid[1]} />,
+    <ProjectsGrid key={3} grid={OurProjectsGrid[2]} />,
   ];
 
   return (
     <section className="p-8 lg:p-24 our-projects">
       <div className="grid grid-cols-12 items-end gap-6">
-        <div className="col-span-12 lg:col-span-6 flex flex-col gap-5">
+        <div className="col-span-12 mb-3 lg:mb-10 lg:col-span-6 flex flex-col gap-5">
           <HeadingCircleLeft text="Our Projects" />
           <Text6Xl text="Creative work." classes={"text-gray-900 font-bold"} />
         </div>
-        <div className="col-span-12 lg:col-span-6">
+        <div className="col-span-12 mb-3 lg:mb-10 lg:col-span-6">
           <TabsUnderlined
-            justify="justify-end"
+            justify="justify-start lg:justify-end"
             tabs={OurProjectsTabs}
             activeTab={activeTab}
             handleTabClick={setActiveTab}
