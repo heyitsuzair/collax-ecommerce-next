8449e90c499b1dc28b036328d028577edac6d2a3
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-01-20 05:55:31 +0500

    Fixes

--- src/config/endpoints/index.js
+++ src/config/endpoints/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.BASE_URL || "http://localhost:1337/";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:1337/";
 export const Endpoints = {
   // Products
   getProducts: BASE_URL + "api/products?populate=*",
