9db25468d8d2ccbeb0e0c3696dfca56111da244a
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-01-20 23:08:45 +0500

    Contact API Integerated

--- src/functions/contact/index.js
+++ src/functions/contact/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { Endpoints, createConfig } from "../../config";
 export const sendMessage = async (submittedData) => {
-  console.log(submittedData);
   try {
     const { data } = await axios.post(
       Endpoints.sendMessage,
